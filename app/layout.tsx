0ca0fa7be1da5af60c03d02f245c07c5d2331e7f
Fritte91 <fredriklindberg1991@gmail.com>
2025-06-25 13:01:30 +0200

    added analytics and speed insight for vercel.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { AppProvider } from '@/app/hooks/use-app'
 import { Providers } from './providers'
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +26,8 @@ export default function RootLayout({
             {children}
           </AppProvider>
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
